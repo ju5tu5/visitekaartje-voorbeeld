68fe03a083e8847af7c74cd48e8ba0e5505bc5b0
ju5tu5 <justus@ju5tu5.nl>
2023-02-20 10:28:10 +0100

    Added a post route

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 // Importeer express uit de node_modules map
 import express from 'express'
 
-const url = 'https://whois.fdnd.nl/api/v1/member/justus-sturkenboom'
-const data = await fetch(url).then((response) => response.json())
-
-// console.log(data.member.name)
-
 // Maak een nieuwe express app aan
 const app = express()
 
@@ -13,13 +8,38 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 // Gebruik de map 'public' voor statische resources
 app.use(express.static('public'))
 
 // Maak een route voor de index
-app.get('/', function (req, res) {
-  // res.send('Hello World!')
-  res.render('index', data)
+app.get('/', function (request, response) {
+  const url = 'https://whois.fdnd.nl/api/v1/member/justus-sturkenboom'
+  fetchJson(url).then((data) => {
+    response.render('index', data)
+  })
+})
+
+app.post('/', function (request, response) {
+  console.log(request.body)
+  const headers = {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify(request.body),
+  }
+  const url = 'https://whois.fdnd.nl/api/v1/shout'
+
+  fetchJson(url, headers).then((data) => {
+    const url = 'https://whois.fdnd.nl/api/v1/member/justus-sturkenboom'
+    fetchJson(url).then((data) => {
+      response.render('index', data)
+    })
+  })
 })
 
 // Stel het poortnummer in waar express op gaat luisteren
@@ -30,3 +50,14 @@ app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url, payload = {}) {
+  return await fetch(url, payload)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
